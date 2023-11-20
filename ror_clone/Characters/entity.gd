c5215fc4222bc40815ee9ba6490d3ac6179baeb0
robotst0p <meyertyler68@gmail.com>
2023-11-20 18:45:03 -0500

    Direction handling, jumping, movement frameworks entity engine

--- ror_clone/Characters/entity.gd
+++ ror_clone/Characters/entity.gd
@@ -5,6 +5,8 @@ class_name entity
 @export var jump_velocity : float = -150.0
 @export var double_jump_velocity : float = -100.0
 
+var direction
+
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var has_double_jumped : bool = false
 
@@ -12,31 +14,28 @@ var has_double_jumped : bool = false
 func _ready():
 	pass # Replace with function body.
 
-func move(delta):
+func move(delta,dir):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	else:
 		has_double_jumped = false
+	if dir:
+		velocity.x = dir * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0 , speed)
+		
+	move_and_slide()
 
-	# Handle Jump.
-	if Input.is_action_just_pressed("jump"):
+#handles jumping 	
+#need to give enemies ability to jump without input 
+func jump(delta):
 		if is_on_floor():
 			velocity.y = jump_velocity
 		elif not has_double_jumped:
 			velocity.y = double_jump_velocity
 			has_double_jumped = true
-			
+#handles moving
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("left", "right")
-	if direction:
-		velocity.x = direction * speed
-	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
-
-	move_and_slide()
-	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func physics_process(delta):
--- ror_clone/Characters/player.gd
+++ ror_clone/Characters/player.gd
@@ -1,5 +1,8 @@
 extends entity
 
-
 func _physics_process(delta):
-	move(delta)
+	direction = Input.get_axis("left", "right")
+	move(delta, direction)
+	if Input.is_action_just_pressed("jump"):
+		jump(delta)
+	
