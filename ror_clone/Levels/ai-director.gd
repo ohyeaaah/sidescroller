242c02808d519e6944038eab592cf708bb667793
robotst0p <meyertyler68@gmail.com>
2023-11-21 12:44:51 -0500

    Ai Director Updates
    basic spawning functionality added based on how many enemies are near the player, correctly sets and spawns within range of viewport

--- ror_clone/Levels/ai-director.gd
+++ ror_clone/Levels/ai-director.gd
@@ -1,11 +1,53 @@
 extends Node2D
-
+#(19,4) (-28, 4)  if (enemy.position.x - (spawn_area.x/2) > 0) 
 #get position of player
 @onready var player_pos = get_parent().get_node("entity/Player").position 
+#size of viewport should be (288, 162) for spawning area
+@onready var spawn_area = get_viewport_rect().size
+
+#since the player is always in the center of the viewport, the visible area around the player extends by 144 x and 81 y
 
 #get number of enemies in scene
 #hook up to signal when enemy spawns to get new list of active enemiess
 @onready var enemy_list = get_parent().get_node("entity").get_children()
+
+#preload enemy type for spawning
+const enemy_type = preload("res://Characters/enemy_1.tscn")
+var prox_enemies : int = 0
+
+#this value will be changed by the difficulty scaling
+var enemy_limit : int = 5
+
+var rng = RandomNumberGenerator.new()
+#function to change the direction of the enemy based on where they are in relation to the player
+func change_dir():
+	for enemy in enemy_list:
+		if enemy.position.x > player_pos.x:
+			enemy.direction = - 1
+		else:
+			enemy.direction = 1
+
+#maybe only call this when an enemy dies, or based on difficulty
+func spawn_manager():
+	#pick random spawn locations inside viewport to spawn enemies
+	var spawn_loc_x = rng.randi_range(player_pos.x - (spawn_area.x/2), player_pos.x + (spawn_area.x/2))
+	
+	for enemy in enemy_list:
+		if (enemy.position.x - (spawn_area.x/2) < player_pos.x):
+			prox_enemies += 1 
+		elif (enemy.position.x + (spawn_area.x/2) > player_pos.x):
+			prox_enemies += 1
+			
+	if prox_enemies <= enemy_limit:
+		var enemy = enemy_type.instantiate()
+		get_parent().get_node("entity").add_child(enemy)
+		
+		enemy.position.x = spawn_loc_x
+		#will have to change vertical spawning later to account for flying enemies
+		enemy.position.y = player_pos.y
+		
+		enemy_list.append(enemy)
+	
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#first entity in list will always be the player --> get rid of it 
@@ -14,9 +56,6 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	player_pos = get_parent().get_node("entity/Player").position 
-	for enemy in enemy_list:
-		if enemy.position.x > player_pos.x:
-			enemy.direction = - 1
-		else:
-			enemy.direction = 1
+	change_dir()
+	spawn_manager()
 	#print(player_pos)
